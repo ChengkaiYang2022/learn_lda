bcd528c3997c5ac5e26101a6bb82910d8e34a77d
kk <18810276930@163.com>
2018-06-27 09:32:33 +0800

    写入到函数中

--- chinese_lda.py
+++ chinese_lda.py
@@ -1,7 +1,10 @@
 from gensim.corpora import Dictionary
+import jieba
+import gensim
+
 
 def get_stop_words(file_name = "stop_words.txt"):
-    f = open(file_name, "r",encoding="utf-8")
+    f = open(file_name, "r", encoding="utf-8")
     result = list()
     for line in f.readlines():
         line = line.strip()
@@ -9,26 +12,22 @@ def get_stop_words(file_name = "stop_words.txt"):
     return result
 
 
-import jieba
-line1 = " 你好，我前两天被xxx骗了，交了100元的手续费，第二天给我打电话让我卡里存5万元，我感觉是骗子，就告诉他不贷款了，但是她说我违约了，要交1000元违约金，合同自然解除，我要了两次账号那边就关机了，一直联系不上，我合同上贷款5万，那边能不能起诉我啊"
-
 # 切词
-tokens = list(jieba.cut(line1))
-print(tokens)
+def lda(line):
+    tokens = list(jieba.cut(line))
 
-stop_word = get_stop_words()
-print("不得已" in stop_word)
-stopped_tokens = [i for i in tokens if i not in stop_word]
-dictionary = Dictionary([stopped_tokens])
-print((dictionary))
+    stop_word = get_stop_words()
+    stopped_tokens = [i for i in tokens if i not in stop_word]
+    dictionary = Dictionary([stopped_tokens])
 
-# 做语料库
-doc = [dictionary.doc2bow(list(jieba.cut(line1)))]
-print(doc)
+    # 做语料库
+    doc = [dictionary.doc2bow(tokens)]
 
-# 做统计
-import gensim
-ldamodel = gensim.models.ldamodel.LdaModel(doc, num_topics=3, id2word=dictionary, passes=20)
+    # 做统计
+    ldamodel = gensim.models.ldamodel.LdaModel(doc, num_topics=2, id2word=dictionary, passes=20)
+    result = list()
+    for i in (ldamodel.print_topics(num_topics=2, num_words=10)):
+        result.append(str(i[1]))
+    return str(result)
 
-for i in (ldamodel.print_topics(num_topics=3, num_words=8)):
-    print(i)
+
